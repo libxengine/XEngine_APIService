5504d2a176ee7d39a0192d37c107210259e95d88
qyt <486179@qq.com>
2022-03-01 14:45:24 +0800

    added:client example

---
+++ XEngine_APPClient/APPClient_HTTPExample/APPClient_HTTPExample.cpp
@@ -0,0 +1,46 @@
+﻿#ifdef _MSC_BUILD
+#include <Windows.h>
+#include <tchar.h>
+#pragma comment(lib,"Ws2_32")
+#pragma comment(lib,"XEngine_BaseLib/XEngine_BaseLib")
+#pragma comment(lib,"XEngine_NetHelp/NetHelp_APIHelp")
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <XEngine_Include/XEngine_CommHdr.h>
+#include <XEngine_Include/XEngine_ProtocolHdr.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Define.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Error.h>
+#include <XEngine_Include/XEngine_NetHelp/APIHelp_Define.h>
+#include <XEngine_Include/XEngine_NetHelp/APIHelp_Error.h>
+
+//需要优先配置XEngine
+//WINDOWS支持VS2022 x86 debug 编译调试
+//linux使用下面的命令编译
+//g++ -std=c++17 -Wall -g APPClient_HTTPExample.cpp -o APPClient_HTTPExample.exe -L /usr/local/lib/XEngine_Release/XEngine_BaseLib -L /usr/local/lib/XEngine_Release/XEngine_NetHelp -lXEngine_BaseLib -lNetHelp_APIHelp -ljsoncpp
+
+int main()
+{
+#ifdef _MSC_BUILD
+	WSADATA st_WSAData;
+	WSAStartup(MAKEWORD(2, 2), &st_WSAData);
+#endif
+	int nLen = 0;
+	TCHAR *ptszMsgBuffer = NULL;
+	LPCTSTR lpszUrl = _T("http://127.0.0.1:5501/ip?function=ipquery&params1=1.29.164.255");
+	
+	if (!APIHelp_HttpRequest_Get(lpszUrl, &ptszMsgBuffer, &nLen))
+	{
+		printf("发送投递失败！\n");
+		return 0;
+	}
+	printf("接受到数据,大小:%d,内容:%s\n", nLen, ptszMsgBuffer);
+	BaseLib_OperatorMemory_FreeCStyle((XPPMEM)&ptszMsgBuffer);
+
+#ifdef _MSC_BUILD
+	WSACleanup();
+#endif
+	return 0;
+}
