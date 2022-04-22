7e13133c506c14d59afa77ea3ee6d036bc3d3763
qyt <486179@qq.com>
2022-04-22 10:07:26 +0800

    added:zodiac of the plugin module

---
+++ XEngine_Source/XEngine_PluginModule/ModulePlugin_Zodiac/PluginZodiac_Error.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/04/21  13:14:06
+//    File Name:   D:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Zodiac\PluginZodiac_Error.h
+//    File Path:   D:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Zodiac
+//    File Base:   PluginZodiac_Error
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     生肖插件导出错误
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                       错误定义
+//////////////////////////////////////////////////////////////////////////
+#define ERROR_XENGINE_APISERVICE_PLUGIN_MODULE_ZODIAC_PARAMENT 0x11001    //参数错误
