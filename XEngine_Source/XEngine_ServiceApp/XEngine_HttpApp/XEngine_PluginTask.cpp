32e94f447d3a286eeb32850151da320de5dfdbe5
qyt <486179@qq.com>
2022-04-23 11:15:24 +0800

    fixed:exection plugin failed

--- XEngine_Source/XEngine_ServiceApp/XEngine_HttpApp/XEngine_PluginTask.cpp
+++ XEngine_Source/XEngine_ServiceApp/XEngine_HttpApp/XEngine_PluginTask.cpp
@@ -12,7 +12,6 @@
 *********************************************************************/
 BOOL XEngine_PluginTask_Handle(LPCTSTR lpszMethodName, LPCTSTR lpszClientAddr, LPCTSTR lpszRVBuffer, int nRVLen, TCHAR*** ppptszList, int nListCount)
 {
-	int nHTTPCode = 0;
 	int nMsgLen = 0;
 	int nPktLen = 0;
 	TCHAR tszMsgBuffer[2048];
@@ -23,7 +22,10 @@ BOOL XEngine_PluginTask_Handle(LPCTSTR lpszMethodName, LPCTSTR lpszClientAddr, L
 	memset(tszPktBuffer, '\0', sizeof(tszPktBuffer));
 	memset(&st_HDRParament, '\0', sizeof(RFCCOMPONENTS_HTTP_HDRPARAM));
 
-	ModulePlugin_Loader_Exec(lpszMethodName, ppptszList, nListCount, &nHTTPCode, tszPktBuffer, &nPktLen);
+	st_HDRParament.bIsClose = TRUE;
+	_tcscpy(st_HDRParament.tszMimeType, "json");
+
+	ModulePlugin_Loader_Exec(lpszMethodName, ppptszList, nListCount, &st_HDRParament.nHttpCode, tszPktBuffer, &nPktLen);
 	RfcComponents_HttpServer_SendMsgEx(xhHTTPPacket, tszMsgBuffer, &nMsgLen, &st_HDRParament, tszPktBuffer, nPktLen);
 	XEngine_Network_Send(lpszClientAddr, tszMsgBuffer, nMsgLen);
 	XLOG_PRINT(xhLog, XENGINE_HELPCOMPONENTS_XLOG_IN_LOGLEVEL_INFO, _T("HTTP客户端:%s,请求的方法:%s,由插件模块处理成功"), lpszClientAddr, lpszMethodName);
