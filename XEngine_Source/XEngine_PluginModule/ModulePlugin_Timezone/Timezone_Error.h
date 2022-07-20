ad6006f8e11dcaf0f97e31b7b95b128d0cb418c4
qyt <486179@qq.com>
2022-07-20 11:52:30 +0800

    added:timezone plugin module

---
+++ XEngine_Source/XEngine_PluginModule/ModulePlugin_Timezone/Timezone_Error.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/07/20  09:57:23
+//    File Name:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Timezone\Timezone_Error.h
+//    File Path:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Timezone
+//    File Base:   Timezone_Error
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     时区错误
+//    History:
+*********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                       错误定义
+//////////////////////////////////////////////////////////////////////////
+#define ERROR_XENGINE_APISERVICE_PLUGIN_MODULE_TIMEZONE_PARAMENT 0x13001  //参数错误
---
+++ XEngine_Source/XEngine_PluginModule/ModulePlugin_Timezone/pch.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "Plugin_Timezone/Plugin_Timezone.h"
+/********************************************************************
+//    Created:     2022/07/20  11:44:22
+//    File Name:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Timezone\pch.cpp
+//    File Path:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_Timezone
+//    File Base:   pch
+//    File Ext:    cpp
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     导出实现
+//    History:
+*********************************************************************/
+BOOL Timezone_IsErrorOccur = FALSE;
+DWORD Timezone_dwErrorCode = 0;
+//////////////////////////////////////////////////////////////////////////
+CPlugin_Timezone m_PluginTimes;
+//////////////////////////////////////////////////////////////////////////
+//                       导出函数定义
+//////////////////////////////////////////////////////////////////////////
+extern "C" DWORD PluginCore_GetLastError()
+{
+	return Timezone_dwErrorCode;
+}
+/*********************************************************************************
+*                                导出函数定义                                    *
+*********************************************************************************/
+extern "C" BOOL PluginCore_Init(LPVOID lParam)
+{
+	return m_PluginTimes.PluginCore_Init(lParam);
+}
+extern "C" BOOL PluginCore_UnInit()
+{
+	return m_PluginTimes.PluginCore_UnInit();
+}
+extern "C" BOOL PluginCore_Call(TCHAR * **pppHDRList, int nListCount, int* pInt_HTTPCode, TCHAR * ptszMsgBuffer, int* pInt_MsgLen, LPCTSTR lpszMsgBuffer, int nMsgLen)
+{
+	return m_PluginTimes.PluginCore_Call(pppHDRList, nListCount, pInt_HTTPCode, ptszMsgBuffer, pInt_MsgLen, lpszMsgBuffer, nMsgLen);
+}
