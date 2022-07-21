6cad0cbbb159767b18e1ba68ebe4577a9da7fe1d
qyt <486179@qq.com>
2022-07-21 10:22:20 +0800

    added:bmi module

---
+++ XEngine_Source/XEngine_PluginModule/ModulePlugin_BMIndex/Plugin_BMIndex/Plugin_BMIndex.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+/********************************************************************
+//    Created:     2022/07/20  16:36:59
+//    File Name:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_BMIndex\Plugin_BMIndex\Plugin_BMIndex.h
+//    File Path:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_BMIndex\Plugin_BMIndex
+//    File Base:   Plugin_BMIndex
+//    File Ext:    h
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     标准身高体重
+//    History:
+*********************************************************************/
+
+class CPlugin_BMIndex
+{
+public:
+	CPlugin_BMIndex();
+	~CPlugin_BMIndex();
+public:
+	BOOL PluginCore_Init(LPVOID lParam);
+	BOOL PluginCore_UnInit();
+	BOOL PluginCore_Call(TCHAR*** pppHDRList, int nListCount, int* pInt_HTTPCode, TCHAR* ptszMsgBuffer, int* pInt_MsgLen, LPCTSTR lpszMsgBuffer, int nMsgLen);
+protected:
+	BOOL Plugin_Timezone_BMIndex(LPCTSTR lpszHigh, LPCTSTR lpszWeight, TCHAR* ptszMsgBufer, int* pInt_Len);
+private:
+};
---
+++ XEngine_Source/XEngine_PluginModule/ModulePlugin_BMIndex/pch.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "Plugin_BMIndex/Plugin_BMIndex.h"
+/********************************************************************
+//    Created:     2022/07/20  19:18:15
+//    File Name:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_BMIndex\pch.cpp
+//    File Path:   H:\XEngine_APIService\XEngine_Source\XEngine_PluginModule\ModulePlugin_BMIndex
+//    File Base:   pch
+//    File Ext:    cpp
+//    Project:     XEngine(网络通信引擎)
+//    Author:      qyt
+//    Purpose:     导出实现
+//    History:
+*********************************************************************/
+BOOL BMIndex_IsErrorOccur = FALSE;
+DWORD BMIndex_dwErrorCode = 0;
+//////////////////////////////////////////////////////////////////////////
+CPlugin_BMIndex m_PluginBMIndex;
+//////////////////////////////////////////////////////////////////////////
+//                       导出函数定义
+//////////////////////////////////////////////////////////////////////////
+extern "C" DWORD PluginCore_GetLastError()
+{
+	return BMIndex_dwErrorCode;
+}
+/*********************************************************************************
+*                                导出函数定义                                    *
+*********************************************************************************/
+extern "C" BOOL PluginCore_Init(LPVOID lParam)
+{
+	return m_PluginBMIndex.PluginCore_Init(lParam);
+}
+extern "C" BOOL PluginCore_UnInit()
+{
+	return m_PluginBMIndex.PluginCore_UnInit();
+}
+extern "C" BOOL PluginCore_Call(TCHAR * **pppHDRList, int nListCount, int* pInt_HTTPCode, TCHAR * ptszMsgBuffer, int* pInt_MsgLen, LPCTSTR lpszMsgBuffer, int nMsgLen)
+{
+	return m_PluginBMIndex.PluginCore_Call(pppHDRList, nListCount, pInt_HTTPCode, ptszMsgBuffer, pInt_MsgLen, lpszMsgBuffer, nMsgLen);
+}
