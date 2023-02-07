80ded772bffbf570e6435b17280109b230c3899d
qyt <486179@qq.com>
2023-02-07 11:05:13 +0800

    update:vscopy

--- XEngine_Source/VSCopy-Debug.bat
+++ XEngine_Source/VSCopy-Debug.bat
@@ -6,6 +6,7 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_OPenSsl.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_SystemApi.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\NetHelp_APIHelp.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XClient_Socket.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_DataBase.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\HelpComponents_Packets.dll" "./"
