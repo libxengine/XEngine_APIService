3b70b5844ea8916b37ee2b7f07d18f57cbb36722
qyt <486179@qq.com>
2024-05-10 09:40:49 +0800

    added:oil info query example

---
+++ XEngine_APPClient/APPClient_OilQuery/APPClient_OilQuery.cpp
@@ -0,0 +1,47 @@
+﻿#ifdef _MSC_BUILD
+#include <Windows.h>
+#include <tchar.h>
+#pragma comment(lib,"Ws2_32")
+#pragma comment(lib,"XEngine_BaseLib/XEngine_BaseLib")
+#pragma comment(lib,"XEngine_Client/XClient_APIHelp")
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <XEngine_Include/XEngine_CommHdr.h>
+#include <XEngine_Include/XEngine_ProtocolHdr.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Define.h>
+#include <XEngine_Include/XEngine_BaseLib/BaseLib_Error.h>
+#include <XEngine_Include/XEngine_Client/APIClient_Define.h>
+#include <XEngine_Include/XEngine_Client/APIClient_Error.h>
+
+//需要优先配置XEngine
+//WINDOWS支持VS2022 x64 debug 编译调试
+//linux使用下面的命令编译
+//g++ -std=c++17 -Wall -g APPClient_OilQuery.cpp -o APPClient_OilQuery.exe -lXEngine_BaseLib -lXClient_APIHelp
+
+int main()
+{
+#ifdef _MSC_BUILD
+	WSADATA st_WSAData;
+	WSAStartup(MAKEWORD(2, 2), &st_WSAData);
+#endif
+	int nLen = 0;
+	XCHAR* ptszMsgBuffer = NULL;
+	//四川
+	LPCXSTR lpszUrl = _X("http://127.0.0.1:5501/api?function=oil&param=%E5%9B%9B%E5%B7%9D");
+
+	if (!APIClient_Http_Request(_X("GET"), lpszUrl, NULL, NULL, &ptszMsgBuffer, &nLen))
+	{
+		printf("发送投递失败！\n");
+		return 0;
+	}
+	printf("接受到数据,大小:%d,内容:%s\n", nLen, ptszMsgBuffer);
+	BaseLib_OperatorMemory_FreeCStyle((XPPMEM)&ptszMsgBuffer);
+
+#ifdef _MSC_BUILD
+	WSACleanup();
+#endif
+	return 0;
+}
