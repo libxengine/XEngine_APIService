44bbe1e42b3dcfc2ab648f9b197deff2483d9de5
qyt <486179@qq.com>
2024-06-27 10:53:18 +0800

    update:sql file

--- XEngine_SQL/XEngine_APIMachine.sql
+++ XEngine_SQL/XEngine_APIMachine.sql
@@ -3,15 +3,15 @@
 
  Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 80036 (8.0.36-0ubuntu0.22.04.1)
+ Source Server Version : 80037 (8.0.37-0ubuntu0.22.04.3)
  Source Host           : 10.0.3.154:3306
  Source Schema         : XEngine_APIMachine
 
  Target Server Type    : MySQL
- Target Server Version : 80036 (8.0.36-0ubuntu0.22.04.1)
+ Target Server Version : 80037 (8.0.37-0ubuntu0.22.04.3)
  File Encoding         : 65001
 
- Date: 16/05/2024 16:37:15
+ Date: 20/06/2024 17:32:50
 */
 
 SET NAMES utf8mb4;
@@ -29,8 +29,9 @@ CREATE TABLE `XEngine_MachineList`  (
   `tszMachineSystem` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '系统名称',
   `tszMachineText` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '机器信息',
   `nTimeNumber` bigint NOT NULL COMMENT '启动次数',
+  `tszLastTime` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `tszCreateTime` datetime NOT NULL COMMENT '注册时间',
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
