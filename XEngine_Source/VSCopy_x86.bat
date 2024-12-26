9994604b148702cec911c757a447262418b646ae
qyt <486179@qq.com>
2024-12-26 17:34:20 +0800

    update:vs copy file

--- XEngine_Source/VSCopy_x86.bat
+++ XEngine_Source/VSCopy_x86.bat
@@ -3,7 +3,7 @@ copy /y "%XEngine_Lib32%\XEngine_BaseLib\XEngine_BaseSafe.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_BaseLib\XEngine_Algorithm.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_Core.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_ManagePool.dll" "./"
-copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_OPenSsl.dll" "./"
+copy /y "%XEngine_Lib32%\XEngine_Core\XEngine_Cryption.dll" "./"
 
 copy /y "%XEngine_Lib32%\XEngine_Client\XClient_APIHelp.dll" "./"
 copy /y "%XEngine_Lib32%\XEngine_Client\XClient_Stream.dll" "./"
