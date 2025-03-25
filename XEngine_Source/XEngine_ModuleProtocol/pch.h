7254384df4990b0a7fed642ade06adcbffd2f9de
qyt <486179@qq.com>
2025-03-25 15:56:18 +0800

    fixed:windows build failure

--- XEngine_Source/XEngine_ModuleProtocol/pch.h
+++ XEngine_Source/XEngine_ModuleProtocol/pch.h
@@ -65,19 +65,19 @@ extern XLONG ModuleProtocol_dwErrorCode;
 #pragma comment(lib,"XEngine_SystemSdk/XEngine_SystemApi")
 #ifdef _DEBUG
 #ifdef _M_X64
-#pragma comment(lib,"../../x64/Debug/jsoncpp")
+#pragma comment(lib,"../x64/Debug/jsoncpp")
 #elif _M_ARM64
-#pragma comment(lib,"../../ARM64/Debug/jsoncpp")
+#pragma comment(lib,"../ARM64/Debug/jsoncpp")
 #elif _M_IX86
-#pragma comment(lib,"../../Debug/jsoncpp")
+#pragma comment(lib,"../Debug/jsoncpp")
 #endif
 #else
 #ifdef _M_X64
-#pragma comment(lib,"../../x64/Release/jsoncpp")
+#pragma comment(lib,"../x64/Release/jsoncpp")
 #elif _M_ARM64
-#pragma comment(lib,"../../ARM64/Release/jsoncpp")
+#pragma comment(lib,"../ARM64/Release/jsoncpp")
 #elif _M_IX86
-#pragma comment(lib,"../../Release/jsoncpp")
+#pragma comment(lib,"../Release/jsoncpp")
 #endif
 #endif
 #endif
