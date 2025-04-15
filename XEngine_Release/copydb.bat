e7a6705a4005317381604131f743e7a8e29ef8ef
qyt <486179@qq.com>
2025-04-15 11:17:03 +0800

    modify:delete db file for release dir and copy db file though script

---
+++ XEngine_Release/copydb.bat
@@ -0,0 +1,4 @@
+copy /y "..\XEngine_Source\XEngine_DBDepend\XEngine_IPMacData\XEngine_DBFile\GeoLite2-ASN.mmdb" "./XEngine_DBFile"
+copy /y "..\XEngine_Source\XEngine_DBDepend\XEngine_IPMacData\XEngine_DBFile\GeoLite2-City.mmdb" "./XEngine_DBFile"
+copy /y "..\XEngine_Source\XEngine_DBDepend\XEngine_IPMacData\XEngine_DBFile\macaddr.xdb" "./XEngine_DBFile"
+copy /y "..\XEngine_Source\XEngine_DBDepend\XEngine_PhoneData\Data\phone_utf.dat" "./XEngine_DBFile/phone.xdb"
